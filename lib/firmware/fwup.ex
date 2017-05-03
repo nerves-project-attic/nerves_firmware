e22c23e53a1bae835e4eda03b9e384e8431f5f9f
Justin Schneck <jschneck@mac.com>
2017-05-02 22:15:38 -0400

    Add streaming interface to fwup

--- lib/firmware/fwup.ex
+++ lib/firmware/fwup.ex
@@ -1,12 +1,77 @@
 defmodule Nerves.Firmware.Fwup do
-  # Trivial wrapper around Frank Hunleth's FWUP package. Someday, this may be
-  # useful as it's own module -- a porcelain for `fwup`, but it would need a lot
-  # of cleanup and a much better API for streaming and return values.
-
-  @moduledoc false
-  @fwup_prog "fwup"
-
+  use GenServer
   require Logger
+
+  def start_link(opts \\ []) do
+    opts = Keyword.put_new(opts, :callback, self())
+    GenServer.start_link(__MODULE__, opts)
+  end
+
+  def stop(pid) do
+    GenServer.stop(pid)
+  end
+
+  def stream_chunk(pid, chunk, opts \\ [await: false]) do
+    Logger.debug "Sending Chunk: #{inspect chunk}"
+    GenServer.call(pid, {:stream_chunk, chunk, opts})
+  end
+
+  def init(opts) do
+    Process.flag(:trap_exit, true)
+    device = opts[:device] || Application.get_env(:nerves_firmware, :device, "/dev/mmcblk0")
+    task = opts[:task] || "upgrade"
+    fwup = System.find_executable("fwup")
+    callback = opts[:callback]
+    port = Port.open({:spawn_executable, fwup},
+      [{:args, ["-F", "-d", device, "-a", "-t", task]},
+        {:packet, 4},
+        :use_stdio,
+        :binary,
+        :exit_status])
+    {:ok, %{
+      port: port,
+      byte_size: 0,
+      callback: callback
+    }}
+  end
+
+  def handle_call({:stream_chunk, chunk, opts}, from, s) do
+    send s.port, {self(), {:command, chunk}}
+    case opts[:await] do
+      true -> {:noreply, %{s | callback: from}}
+      false -> {:reply, :ok, s}
+    end
+  end
+
+  def handle_info({_port, {:data, <<"OK", code :: integer-16>>}}, s) do
+    Logger.debug "FWUP Done"
+    GenServer.reply(s.callback, :ok)
+    {:noreply, s}
+  end
+
+  def handle_info({_port, {:data, <<"ER", code :: integer-16, message :: binary>>}}, s) do
+    Logger.debug "FWUP Error #{code}: #{message}"
+    {:noreply, s}
+  end
+
+  def handle_info({_port, {:data, <<"WN", code :: integer-16, message :: binary>>}}, s) do
+    Logger.debug "FWUP Warning #{code}: #{message}"
+    {:noreply, s}
+  end
+
+  def handle_info({_port, {:data, <<"PR", progress :: integer-16>>}}, s) do
+    Logger.debug "FWUP Progress: #{progress}%"
+    {:noreply, s}
+  end
+
+  def handle_info({_port, {:data, resp}}, s) do
+    Logger.debug "FWUP unknown response: #{inspect resp}"
+    {:noreply, s}
+  end
+
+  def handle_info(msg, s) do
+    {:noreply, s}
+  end
 
   @doc """
   Apply the firmware in <input> to the given <device>, executing <task>.
@@ -24,7 +89,7 @@ defmodule Nerves.Firmware.Fwup do
     Logger.info "Firmware: applying #{task} to #{device}"
     fwup_args =
       ["-aqU", "--no-eject", "-i", input, "-d", device, "-t", task] ++ args
-    case System.cmd(@fwup_prog, fwup_args) do
+    case System.cmd("fwup", fwup_args) do
       {_out, 0} ->
         :ok
       {error, _} ->
